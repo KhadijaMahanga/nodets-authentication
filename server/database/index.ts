90f078375ef4ea005132dae82bbad09e4670f456
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-05-21 14:59:10 +0300

    initialize db connection

---
+++ server/database/index.ts
@@ -0,0 +1,13 @@
+import { Dialect, Sequelize } from 'sequelize';
+import connection from './config';
+
+const { database, user, password, host, dbLogging } = connection;
+
+// create a sequalize connection
+const sequelizeConnection = new Sequelize(database, user, password, {
+	host,
+	logging: dbLogging,
+	dialect: 'postgres' as Dialect,
+});
+
+export default sequelizeConnection;
