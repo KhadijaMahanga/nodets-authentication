c9d37b250819ff6db14ab348c98db0c8dc778967
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-05-21 15:59:43 +0300

    clean up entry file

---
+++ server/config.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: '${__dirname}/../.env'
+});
+
+export const port: number = parseInt(process.env.PORT as string, 10) || 7000;
+
+export const db_host: string = String(process.env.DB_HOST);
+export const db_port: number = Number(process.env.DB_PORT);
+export const db_name: string = String(process.env.DB_NAME);
+export const db_user: string = String(process.env.DB_USER);
+export const db_password: string = String(process.env.DB_PASSWORD);
+
+
+export const node_env = process.env.NODE_ENV;
+export const log = process.env.LOG;
--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,44 @@
 /** Import Modules*/
-
-// import * as dotenv from "dotenv";
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-// dotenv.config();
+import { port } from './config';
+import sequelizeConnection from "./database";
 
-/**
- * App Variables
- */
- 
- const PORT: number = parseInt(process.env.PORT as string, 10) || 7000;
- 
- const app = express();
+// New Express application
+const app: Express = express();
 
-/**
- *  App Configuration
- */
+// App Configuration
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).send({ message: `User Authentication API` })
+})
+
+try {
+  
+} catch (error: any) {
+    console.log(`Error occurred: ${error.message}`)
+}
+
+const start = async (): Promise<void> => {
+  try {
+    await sequelizeConnection.authenticate();
+
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+
+    //create table on db
+    await sequelizeConnection.sync();
+    console.log('Connection to Database has been established successfully.');
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+void start()
