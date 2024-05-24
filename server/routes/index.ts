4cbf702fbb9037af29476d46a4448d9f0e4411d8
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-05-24 12:22:34 +0300

    add routes skeleton

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,9 +3,9 @@ import express from 'express';
 import user from './user.routes';
 import auth from './auth.routes';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/healthcheck", (_, res) => res.sendStatus(200));
+router.get('/healthcheck', (_, res) => res.sendStatus(200));
 router.use(user);
 router.use(auth);
 
