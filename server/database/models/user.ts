81a14e856e0a5db44ac2edb2f4545073e9ab54a8
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-05-24 08:57:44 +0300

    re-define user model for stricter typechecking

--- server/database/models/user.ts
+++ server/database/models/user.ts
@@ -1,18 +1,29 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
 import sequelizeConnection from '..';
 
-const User = sequelizeConnection.define('User', {
+interface UserModel extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> {
+    //Fields are optional when calling UserModel.create() or UserModel.build()
+    id: CreationOptional<string>;
+    username: string;
+    email: string;
+    password: string;
+
+}
+
+const User = sequelizeConnection.define<UserModel>('User', {
   id: {
     type: DataTypes.UUIDV4,
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
   },
-  name: {
+  username: {
     type: DataTypes.STRING,
+    unique: true,
     allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
+    unique: true,
     allowNull: false,
   },
   password: {
