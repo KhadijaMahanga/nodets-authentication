4683210c86e7403dc9cc54c5be40223546efa369
KhadijaMahanga <khadijamahanga@y7mail.com>
2024-05-29 15:47:50 +0300

    update starting file to use prisma connection

--- server/index.ts
+++ server/index.ts
@@ -4,37 +4,38 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 import { port } from './config';
-import sequelizeConnection from './src/database';
+import prisma from './src/database';
 import router from './src/routes';
 
 // New Express application
 const app: Express = express();
 
-// App Configuration
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+async function main () {
+  // App Configuration
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+  
+  //register routes
+  app.use(router);
+  
+  app.get('/', (req: Request, res: Response) => {
+    return res.status(200).send({ message: `User Authentication API` });
+  });
 
-app.use(router);
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 
-app.get('/', (req: Request, res: Response) => {
-  return res.status(200).send({ message: `User Authentication API` });
-});
+}
 
-const start = async (): Promise<void> => {
-  try {
-    await sequelizeConnection.authenticate();
 
-    app.listen(port, () => {
-      console.log(`Listening on port ${port}`);
-    });
-
-    //create table on db
-    await sequelizeConnection.sync();
-    console.log('Connection to Database has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-};
-
-void start();
+main()
+  .then(async () => {
+    await prisma.$connect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
